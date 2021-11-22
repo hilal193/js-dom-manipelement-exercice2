3931e1ea08cc0a13f78bb02af9cf7361667d6053
Hilal Koujil <hilal1993@hotmail.be>
2021-11-22 11:40:06 +0100

    exo 5

---
+++ main.js
@@ -0,0 +1,15 @@
+// 1
+let premier = document.querySelector("p");
+let premierClass = document.getElementsByClassName("tartine");
+console.log(premierClass);
+
+// 2
+let deux = document.querySelector("p");
+let deuxId = document.getElementById("toast");
+console.log(deuxId);
+
+// 3
+let trois= document.querySelector("p");
+let troisDernier = document.getElementsByClassName("tartine");
+console.log(troisDernier);
+
